e3158645cda898b046973dff4e1c39ea803b6d82
Shane Lessard <lessardshane@gmail.com>
2016-02-02 13:30:45 -0500

    Add option to use curl SSL version 6 (tls 1.2)

--- src/IpnListener.php
+++ src/IpnListener.php
@@ -64,6 +64,13 @@ class IpnListener
      */
     public $verify_ssl = true;
 
+    /**
+     * If true, declase curl SSL version 6 (tls 1.2)
+     * 
+     * @var bool
+     */
+    public $use_tls = false;
+
     private $_errors = array();
     private $post_data;
     private $rawPostData;				// raw data from php://input
@@ -97,6 +104,10 @@ class IpnListener
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
             curl_setopt($ch, CURLOPT_CAINFO, dirname(dirname(__FILE__)) . '/cert/api_cert_chain.crt');
+        }
+
+        if ($this->use_tls){
+            curl_setopt($ch, CURLOPT_SSLVERSION, 6);
         }
 
         curl_setopt($ch, CURLOPT_URL, $uri);
